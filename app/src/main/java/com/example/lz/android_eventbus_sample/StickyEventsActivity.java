0f354fe22170553dc3f148ba3c8c816b76def3f7
lizhen <lizhen0211@163.com>
2018-11-01 13:31:01 +0800

    stickyEvents

--- app/src/main/java/com/example/lz/android_eventbus_sample/StickyEventsActivity.java
+++ app/src/main/java/com/example/lz/android_eventbus_sample/StickyEventsActivity.java
@@ -2,8 +2,7 @@ package com.example.lz.android_eventbus_sample;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
+import android.util.Log;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
@@ -11,10 +10,25 @@ import org.greenrobot.eventbus.ThreadMode;
 
 public class StickyEventsActivity extends Activity {
 
+    private static final String TAG = StickyEventsActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sticky_events);
+
+        //手动获取和删除粘性事件
+        //Getting and Removing sticky Events manually
+        MessageEvent stickyEvent = EventBus.getDefault().getStickyEvent(MessageEvent.class);
+        Log.e(TAG, "getting:" + stickyEvent.message);
+        // Better check that an event was actually posted before
+        if (stickyEvent != null) {
+            // "Consume" the sticky event
+            //要删除（消耗）粘性事件，以便它们不再被传递
+            EventBus.getDefault().removeStickyEvent(stickyEvent);
+            Log.e(TAG, "remove:" + stickyEvent.message);
+            // Now do something with it
+        }
     }
 
     @Override
@@ -28,7 +42,7 @@ public class StickyEventsActivity extends Activity {
     // UI updates must run on MainThread
     @Subscribe(sticky = true, threadMode = ThreadMode.MAIN)
     public void onEvent(MessageEvent event) {
-        Toast.makeText(this, event.message, Toast.LENGTH_SHORT).show();
+        Log.e(TAG, "onEvent:" + event.message);
     }
 
     @Override
